a1fa35b3509d7a4951cb3ce2e68d8afb9c1b8206
Vaibhav Kamble <vk751999@gmail.com>
2024-04-12 20:02:51 +0530

    added constants

---
+++ Smarty.Business/Constants/SmartyConstants.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smarty.Business.Constants
+{
+    public class SmartyConstants
+    {
+        public static string BaseUrl ="BaseUrl";
+        public static string AuthId = "AuthId";
+        public static string AuthToken = "AuthToken";
+        public static string Match = "Match";
+    }
+}
